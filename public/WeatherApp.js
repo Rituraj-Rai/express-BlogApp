ce69b520ecfd09ca6ecff2922c88a6b360d776d4
Rituraj <stu.rituraj@gmail.com>
2023-03-16 23:35:52 +0530

    adding user ip display & log feature

--- public/WeatherApp.js
+++ public/WeatherApp.js
@@ -3,6 +3,7 @@ const city = document.querySelector("h2");
 const img = document.querySelector("img");
 const tmp = document.querySelector("span");
 const p = document.querySelector("div p");
+const ipspan = document.getElementById("ip");
 
 
 const success = async (pos) => {
@@ -21,7 +22,7 @@ const getInfo = async(loc) => {
     try {
         const { coords: {latitude, longitude} } = loc; //destructuring
         const res = await axios.get(`/weather/${latitude}/${longitude}`) // calling the api from server
-        const { data } = res; //geting only data from response
+        const { data } = res; //geting only data from response        
         fill(data);
         console.log(data); 
     } catch (err) {
@@ -36,5 +37,6 @@ function fill(data){
     p.innerText = `${data.weather[0].description}`;
     lis[0].innerText = `Humidity ${data.main.humidity}%`
     lis[1].innerText = `Wind Speed: ${data.wind.speed} m/s`
+    ipspan.innerText = data.ip;
     img.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
 }
